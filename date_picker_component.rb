5a328a8300aaf8cc1e4c41544027a97b31edbf79
NasserHumood <nooh43@live.com>
2026-02-23 20:22:48 +0300

    Update date_picker_component.rb
    1. أخر التاريخ فقط واستخراج الباقي منه

--- date_picker_component.rb
+++ date_picker_component.rb
@@ -5,9 +5,14 @@ module Sama
     # ~
     # تعيين القيم الإفتراضية
     # ~
-    def initialize(month: Date.current.month, year: Date.current.year)
-      @month = month
-      @year = year
+    def initialize(date: Date.current, selected: nil)
+      @date = date
+      @selected = selected
+
+      # ~ معالجة مرة واحدة فقط
+      @first_of_chosen_month = @date.beginning_of_month
+      @previous_month_date = @first_of_chosen_month - 1.month
+      @next_month_date = @first_of_chosen_month + 1.month
     end
 
     private
@@ -17,40 +22,59 @@ module Sama
     # ~
     def month_array
       # ~ معلومات عامة عن الشهر
-      first_of_month = Date.new(@year, @month, 1)
-      leading_blanks = first_of_month.wday
-      total_days = Time.days_in_month(@month, @year)
+      leading_blanks = @first_of_chosen_month.wday
+      total_days = Time.days_in_month(@date.month, @date.year)
 
       # ~ معلومات عن الشهر الفائت
-      previous_total = Time.days_in_month(previous_month, previous_year)
+      previous_total_days = Time.days_in_month(@previous_month_date.month, @previous_month_date.year)
 
       # ~ تعيين مصفوفة الأيام السابقة
       previous_days = []
 
       # ~ حساب الأيام الفارغة في البداية
       if leading_blanks > 0
-        first_day = previous_total - leading_blanks + 1
-        (first_day..previous_total).each do |d|
-          previous_days << {day: d, type: :previous}
+        first_day = previous_total_days - leading_blanks + 1
+        (first_day..previous_total_days).each do |d|
+          previous_days << {
+            day: d,
+            type: :previous,
+            datetime: "#{@previous_month_date.year}-#{@previous_month_date.month}-#{d}"
+          }
         end
       end
 
       # ~ أيام الشهر الحالي
-      current_days = (1..total_days).map { |d| {day: d, type: :current} }
+      current_days = (1..total_days).map { |d|
+        {
+          day: d,
+          type: :current,
+          datetime: "#{@date.year}-#{@date.month}-#{d}"
+        }
+      }
 
       # ~ تغيير نوع اليوم الحالي.
-      if (@month == Date.current.month) && (@year == Date.current.year)
+      if (@date.month == Date.current.month) && (@date.year == Date.current.year)
         today_index = Date.current.day
-        current_days[today_index - 1] = {day: today_index, type: :now}
+        current_days[today_index - 1] = {
+          day: today_index,
+          type: :now,
+          datetime: Date.current.to_s
+        }
       end
 
       # ~ تعيين المصفوفة الأساسية
       main_array = previous_days + current_days
 
-      # ~ إضافة أيام الشهر التالي (trailing) لنكمل 42 خلية
+      # ~ إضافة بقية الخلايا لإكمال 42 خلية باستخدام الشهر القادم
       if main_array.length < 42
         difference = 42 - main_array.length
-        next_days = (1..difference).map { |d| {day: d, type: :next} }
+        next_days = (1..difference).map { |d|
+          {
+            day: d,
+            type: :next,
+            datetime: "#{@next_month_date.year}-#{@next_month_date.month}-#{d}"
+          }
+        }
         main_array += next_days
       end
 
@@ -63,52 +87,34 @@ module Sama
     def one_cell(cell)
       day = cell[:day]
       type = cell[:type]
+      datetime = cell[:datetime]
 
       case type
       when :previous, :next
-        tag.button(class: "bg-gray-50 py-1.5 text-gray-400 hover:bg-gray-100 focus:z-10") do
-          # <time datetime="2022-01-02" class="mx-auto flex size-7 items-center justify-center rounded-full">3</time>
-          tag.time(day.to_s, class: "mx-auto flex size-5 items-center justify-center rounded-full")
+        button_to url_for(calendar_update_path),
+          method: :post,
+          params: {date: datetime.to_date, selected: datetime},
+          form_class: "bg-gray-50 py-1.5 text-gray-400 hover:bg-gray-100 focus:z-10" do
+          tag.time(day, datetime: datetime, class: "mx-auto flex size-5 items-center justify-center rounded-full")
         end
+
       when :now
-        tag.button(class: "bg-white py-1.5 font-semibold text-indigo-600 hover:bg-gray-100 focus:z-10") do
-          # <time datetime="2022-01-02" class="mx-auto flex size-7 items-center justify-center rounded-full">3</time>
-          tag.time(day.to_s, class: "mx-auto flex size-5 items-center justify-center rounded-full")
+        button_to url_for(calendar_update_path),
+          method: :post,
+          params: {date: datetime.to_date, selected: datetime},
+          form_class: "bg-white py-1.5 font-semibold text-indigo-600 hover:bg-gray-100 focus:z-10" do
+          tag.time(day, datetime: datetime, class: "mx-auto flex size-5 items-center justify-center rounded-full")
         end
+
       else
-        tag.button(class: "bg-white py-1.5 text-gray-900 hover:bg-gray-100 focus:z-10") do
-          # <time datetime="2022-01-02" class="mx-auto flex size-7 items-center justify-center rounded-full">3</time>
-          tag.time(day.to_s, class: "mx-auto flex size-5 items-center justify-center rounded-full")
+        button_to url_for(calendar_update_path),
+          method: :post,
+          params: {date: datetime.to_date, selected: datetime},
+          form_class: "bg-white py-1.5 text-gray-900 hover:bg-gray-100 focus:z-10" do
+          tag.time(day, datetime: datetime, class: "mx-auto flex size-5 items-center justify-center rounded-full")
         end
+
       end
-    end
-
-    # ~
-    # الشهر الماضي
-    # ~
-    def previous_month
-      (@month == 1) ? 12 : @month - 1
-    end
-
-    # ~
-    # السنة الماضية
-    # ~
-    def previous_year
-      (@month == 1) ? @year - 1 : @year
-    end
-
-    # ~
-    # الشهر القادم
-    # ~
-    def next_month
-      (@month == 12) ? 1 : @month + 1
-    end
-
-    # ~
-    # السنة القائدمة
-    # ~
-    def next_year
-      (@month == 12) ? @year + 1 : @year
     end
   end
 end
